2ef3b9c9a53adf6b3a2efb2414d4ab4e126138c9
NGUYỄN SỸ HẢI THANH <thanhproo1412@gmail.com>
2023-11-20 21:29:00 +0700

    update create home
    update create home

---
+++ firstapp/src/app/app.tsx
@@ -0,0 +1,16 @@
+// pages/app.tsx
+
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="bg-gray-200 p-8">
+      <h1 className="text-4xl font-bold mb-4">Hello Next.js with TypeScript and Tailwind CSS!</h1>
+      <p className="text-lg">
+        This is a simple Next.js app configured with TypeScript and Tailwind CSS.
+      </p>
+    </div>
+  );
+};
+
+export default App;
