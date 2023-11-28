79c68309fcb9adeac8f62b19ce302832ee8dd2dc
NGUYỄN SỸ HẢI THANH <thanhproo1412@gmail.com>
2023-11-28 23:00:25 +0700

    add slider ...

--- secondapp/src/components/Body.tsx
+++ secondapp/src/components/Body.tsx
@@ -1,5 +1,4 @@
-import Image from 'next/image'
-import Link from "next/link";
+import Banner from "./Banner";
 import styles from "./css/BodyCss.module.css";
 
 export const Body = () => {
@@ -7,8 +6,7 @@ export const Body = () => {
   return (
 
     <div className={`"bg-gray-800 px-4 py-6" ${styles.body} `}>
-
-      body
+      <Banner />
     </div>
 
   )
